b8d4907296602d830736e47b576fbf16a481b0ef
sanwaJW <sanwarjayswal@gmail.com>
2024-02-09 17:15:04 +0530

    hope first commit

---
+++ lib/trash/trashTest/login_register.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginRegisterTest extends StatelessWidget {
+  const LoginRegisterTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
